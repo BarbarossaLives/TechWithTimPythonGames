48ba9cb7f5bd3030559f6e31f7fcaf7f6a6857f6
montebruce <montebruce@proton.me>
2025-05-06 19:03:56 -0500

    save

--- GameOfLife/GameOfLife.py
+++ GameOfLife/GameOfLife.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 pygame.init
 BLACK = (0,0,0)
@@ -18,36 +19,89 @@ FPS = 60
 screen = pygame.display.set_mode((WIDTH,HEIGHT))
 
 clock = pygame.time.Clock()
+def gen(num):
+    return set([(random.randrange(0,GRID_HEIGHT), random.randrange(0, GRID_WIDTH))for i in range(num)])
 
 
-def draw_grid(positions):
+def draw_grid(positions) :
     
     for position in positions:
         col, row = position
         top_left = (col * TILE_SIZE, row * TILE_SIZE)
-        pygame.draw.line(screen, YELLOW, *top_left, TILE_SIZE, TILE_SIZE)
-    
-    
+        pygame.draw.rect(screen, YELLOW, (*top_left, TILE_SIZE, TILE_SIZE))
     
     for row in range(GRID_HEIGHT):
         pygame.draw.line(screen, BLACK,(0, row * TILE_SIZE),(WIDTH, row * TILE_SIZE))
     for col in range(GRID_WIDTH):
         pygame.draw.line(screen, BLACK,(col * TILE_SIZE, 0),(col*TILE_SIZE,HEIGHT))
 
+def adjust_grid(positions):
+    all_neighbors = set()
+    new_positions = set()
+    
+    for position in positions:
+        neighbors = get_neighbors(positions)
+        all_neighbors.update(neighbors)
+    
+        neighbors = list(filter (lambda x: x in positions,neighbors))
+    
+        if len(neighbors) in [2,3]:
+                new_positions.add(position)
+        
+    for position in all_neighbors:
+        neighbors = get_neighbors(position)  
+        neighbors = list(filter (lambda x: x in positions,neighbors))
+        
+        if len(neighbors)  == 3:
+            new_positions.add(position)
+            
+    return new_positions
+        
+
+def get_neighbors():
+    pass
+
+
 # Main Loop
 def main():
     running = True
-    
+    playing = False
     positions = set()
-    positions.add((10,10))
+    
     while running:
         clock.tick(FPS)
-        
+    
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
                 
+            if event.type  == pygame.MOUSEBUTTONDOWN:
+                x,y = pygame.mouse.get_pos()
+                col = x//TILE_SIZE
+                row =y//TILE_SIZE
+                pos = (col,row)
+                if pos in positions:
+                    positions.remove(pos)
+                else :
+                    positions.add(pos)
 
+            
+            
+            if event.type == pygame.KEYDOWN:
+                print("hit key")
+                if event.key == pygame.K_SPACE:
+                    playing = not playing
+
+                    
+                if event.key == pygame.K_c:
+                    positions = set()
+                    playing = False
+                
+                if event.key == pygame.K_g:
+                    positions = gen(random.randrange(2,5)* GRID_WIDTH)
+
+                
+            
         screen.fill(GREY)
         draw_grid(positions)
         pygame.display.update()
